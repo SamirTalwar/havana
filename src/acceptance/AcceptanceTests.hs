c090cd77fe99bc6775ae986e6e930b404d3541f0
Samir Talwar <samir@noodlesandwich.com>
2015-02-08 13:23:17 +0000

    Do the diff in Haskell instead of outsourcing.

--- src/acceptance/AcceptanceTests.hs
+++ src/acceptance/AcceptanceTests.hs
@@ -4,10 +4,15 @@
 
 import Shelly
 import Control.Monad
+import qualified Data.ByteString as BS
+import qualified Data.List as L
 import qualified Data.String as S
 import qualified Data.Text as T
+import qualified Data.Word as W
+import qualified Numeric as N
 import qualified System.Directory as Dir
 import qualified System.FilePath as Path
+import Text.Printf (printf)
 
 import qualified Havana.Compiler
 
@@ -60,8 +65,16 @@ execute (TestCase testName directory files) context = do
     chdir directory $ forM_ files $ \file -> do
         javacOutputFile <- compile "javac" file (tmpPath context) (cmd (javac context))
         havanaOutputFile <- compile "havana" file (tmpPath context) (cmd (havanac context))
-        exitCode <- highlightOutput $ cmd "cmp" javacOutputFile havanaOutputFile
-        exitOnFailure exitCode
+        comparisonFailure <- liftIO $ compareFiles javacOutputFile havanaOutputFile
+        case comparisonFailure of
+            Just (byteIndex, byteA, byteB) -> do
+                coloredAs red $ echo $ T.pack $ printf (
+                    "At byte %04d (0x%04x), the values differ.\n"
+                    ++ "  In file %s, it is: %02x\n"
+                    ++ "  In file %s, it is: %02x\n"
+                    ) byteIndex byteIndex (fromPath javacOutputFile) byteA (fromPath havanaOutputFile) byteB
+                quietExit 1
+            Nothing -> return ()
 execute (DisabledTestCase testName) context = coloredAs yellow $ do
     echo_n "DISABLED: "
     echo testName
@@ -73,22 +86,29 @@ compile prefix file outputPath compiler = do
     rm (outputFile file)
     return destination
 
-fromPath = T.unpack . toTextIgnore
+compareFiles :: Shelly.FilePath -> Shelly.FilePath -> IO (Maybe (Int, W.Word8, W.Word8))
+compareFiles fileA fileB = do
+    a <- BS.readFile $ fromPath fileA
+    b <- BS.readFile $ fromPath fileB
+    return $ L.find (\(_, byteA, byteB) -> byteA /= byteB)
+            (zip3 [0..] (pad 0 (BS.length b) (BS.unpack a)) (pad 0 (BS.length a) (BS.unpack b)))
 
-highlightOutput command = do
-    output <- errExit False (silently command)
-    exitCode <- lastExitCode
-    let color = if exitCode > 0 then red else green
-    coloredAs color $ echo_n output
-    return exitCode
-
-exitOnFailure exitCode = when (exitCode > 0) (quietExit exitCode)
+pad :: a -> Int -> [a] -> [a]
+pad value minimumSize list
+    | listSize < minimumSize
+        = replicate (listSize - minimumSize) value ++ list
+    | otherwise
+        = list
+    where
+    listSize = length list
 
 coloredAs color action = do
     echo_n color
     result <- action
     echo_n reset
     return result
+
+fromPath = T.unpack . toTextIgnore
 
 red = "\x1b[31m"
 green = "\x1b[32m"
